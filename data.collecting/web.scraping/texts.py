ab5d081d47fc20e51805ce85d7d29e1b721acc02
Pavel Krusek <pavel.krusek@gmail.com>
2022-08-14 00:30:48 +0200

    * texts

--- data.collecting/web.scraping/texts.py
+++ data.collecting/web.scraping/texts.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 import logging
 import helpers
@@ -18,10 +20,12 @@ def get_text_for(cantata):
     for td in sp_table.find_all('td', class_='text'):
         texts.append(td.text)
 
-    df['Movement'] = movements
-    df['Instruments'] = instruments
-    df['Text'] = texts
-    df['BWV'] = 1
+    tmp_df = pd.DataFrame({'Movement': movements,
+                           'Instruments': instruments,
+                           'Text': texts,
+                           'BWV': cantata})
+
+    return tmp_df
 
 
 if __name__ == '__main__':
@@ -30,13 +34,16 @@ if __name__ == '__main__':
 
     df = pd.DataFrame()
     bwv = pd.read_csv("./_data/cantatas.csv", usecols=[0], header=None)[0].tolist()
-    bwv = bwv[0:1]
+    bwv = bwv[3:5]
 
     for item in bwv:
         try:
-            get_text_for(item)
+            tmp = get_text_for(item)
+            df = pd.concat([df, tmp])
+            logger.info(f'cantata processed {item}')
+            time.sleep(5.0)
         except(Exception,):
             logger.error(f'error in cantata {item}')
             pass
 
-    df.to_csv("./_data/texts.csv")
+    df.to_csv('./_data/texts.csv')
